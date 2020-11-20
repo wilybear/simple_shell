454671f7d15961ef7798d2fce65f9724500209db
wilybear <samerj9712@gmail.com>
2020-11-20 21:20:28 +0900

    pipe and redirection

--- simple_myshell.c
+++ simple_myshell.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <sys/wait.h>
@@ -62,10 +63,18 @@ int main(int argc, char **argv)
 {
   int i = 0;
   pid_t pid;
+  int pipe_bak=0;
   int status;
   int len;
   int state;
+  int pipefd[2];
+  char* input_file;
+  char* output_file;
+  char* detect;
+  char* read;
+  bool last_pipe_flag = false;
   extern int errno;
+  bool piping = false;
   char *cmdvector[MAX_CMD_ARG];
   char cmdline[BUFSIZ];
   bool bgflag = false;
@@ -87,23 +96,45 @@ int main(int argc, char **argv)
   }
   while (1)
   {
-    fputs(prompt, stdout);
-    cmdline[0] = '\0';
-    while (!fgets(cmdline, BUFSIZ, stdin) && !restart)
-      ;
-    if (restart)
-    {
-      restart = false;
-    }
-    cmdline[strlen(cmdline) - 1] = '\0';
-    if (feof(stdin))
-    {
-      printf("\n");
-      exit(0);
+    if(!piping){
+      fputs(prompt, stdout);
+      cmdline[0] = '\0';
+      
+      while (!fgets(cmdline, BUFSIZ, stdin) && !restart)
+        ;
+      if (restart)
+      {
+        restart = false;
+      }
+      cmdline[strlen(cmdline) - 1] = '\0';
+			read = cmdline;
+      if (feof(stdin))
+      {
+        printf("\n");
+        exit(0);
+      }
     }
 
     memset(cmdvector, 0, sizeof(char *) * MAX_CMD_ARG);
-    i = makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
+
+    if(detect = strstr(read,"|")){
+        piping = true;
+        detect[0] = '\0';
+        i = makelist(read, " \t", cmdvector, MAX_CMD_ARG);
+        read = detect + 1;
+        last_pipe_flag = false;
+        pipe(pipefd);
+        if(!pipe_bak){
+          pipe_bak = dup(STDIN_FILENO);
+        }
+    }else{
+    i = makelist(read, " \t", cmdvector, MAX_CMD_ARG);
+      if(piping){
+        last_pipe_flag = true;
+        piping = false;
+      }
+      
+    }
     if (!cmdvector[0])
     {
       continue;
@@ -125,11 +156,13 @@ int main(int argc, char **argv)
       bgflag = true;
       if (!strtok(cmdvector[i - 1], " \t"))
         cmdvector[i - 1] = NULL;
+      i--;
     }
     else
     {
       bgflag = false;
     }
+
 
     switch (pid = fork())
     {
@@ -137,6 +170,8 @@ int main(int argc, char **argv)
       signal(SIGQUIT, SIG_DFL);
       signal(SIGINT, SIG_DFL);
 
+      int fd;
+      int pivot = 0;
       if (bgflag)
       {
         setpgid(0, 0);
@@ -145,13 +180,52 @@ int main(int argc, char **argv)
       {
         tcsetpgrp(STDIN_FILENO, getpgid(0));
       }
+      for(int j =0; j<i;j++){
+          if(strchr(cmdvector[j],'>')!=NULL){
+            if(pivot == 0)
+              pivot = j;
+            output_file = cmdvector[++j];
+            if((fd = open(output_file,O_WRONLY | O_CREAT | O_TRUNC))== -1){
+              perror("open error: ");
+            }
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+          }else if(strchr(cmdvector[j],'<')!=NULL){
+            if(pivot == 0)
+              pivot = j;
+            input_file = cmdvector[++j];
+               if((fd = open(input_file,O_RDONLY))== -1){
+              perror("open error: ");
+            }
+            dup2(fd,STDIN_FILENO);
+            close(fd);
+          }
+      }
+      if(pivot !=0){
+        cmdvector[pivot] = NULL;
+      }
+
+      if(piping){
+          close(pipefd[0]);
+          dup2(pipefd[1],STDOUT_FILENO);
+          close(pipefd[1]);
+      }
+
+
+      
       execvp(cmdvector[0], cmdvector);
 
     case -1:
       fatal("main()");
     default:
+
       if (!bgflag)
-      {
+      {    	
+        if (piping) {
+					close(pipefd[1]);
+					dup2(pipefd[0], STDIN_FILENO);
+					close(pipefd[0]);
+				}else{
         while (1)
         {
           if (waitpid(pid, NULL, 0) == -1)
@@ -171,6 +245,14 @@ int main(int argc, char **argv)
             break;
           }
         }
+        }
+        if (last_pipe_flag) {
+					close(pipefd[0]);
+					dup2(pipe_bak, STDIN_FILENO);
+					close(pipe_bak);
+					pipe_bak = 0;
+					 last_pipe_flag = false;
+				}
         tcsetpgrp(STDIN_FILENO, getpgid(0));
       }
     }
@@ -185,7 +267,8 @@ static void child_handler()
   while ((child_pid = waitpid(-1, 0, WNOHANG)) > 0)
   {
     printf("pid = %ld done \n", (long)child_pid);
-    restart = true;
+
+     restart = true;
   }
 }
 
